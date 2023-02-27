ba751ce8bdb416c0af250994ff54be1b07f629fc
reek004 <chatterjeeswarnabha@gmail.com>
2023-02-27 21:17:25 +0530

    lgtm

--- day_1/2_temp_converter.c
+++ day_1/2_temp_converter.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter the temperature in fahrenheit \n");
     scanf("%f", &fahrenheit);
 
-    printf("The temperature in celcius scale will be %.02f ", (fahrenheit - 32) * 5 / 9);
+    printf("The temperature in celcius scale will be %.02f ", (fahrenheit- 32) * 5 / 9);
 
     return 0;
 }
