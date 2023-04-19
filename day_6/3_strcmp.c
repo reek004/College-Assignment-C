447db333fc53cb243fbb603905eb6e29069ceab1
reek004 <chatterjeeswarnabha@gmail.com>
2023-04-19 14:40:17 +0530

    cool

--- day_6/3_strcmp.c
+++ day_6/3_strcmp.c
@@ -1,31 +1,36 @@
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-  	char Str1[25], Str2[25];
-  	int result, i;
- 
-  	printf("\n Please Enter the First String :  ");
-  	gets(Str1);
-  	
-  	printf("\n Please Enter the Second String :  ");
-  	gets(Str2);
-  	
-  	for(i = 0; Str1[i] == Str2[i] && Str1[i] == '\0'; i++);
-		   
-  	if(Str1[i] < Str2[i])
-   	{
-   		printf("\n str1 is Less than str2");
-	}
-	else if(Str1[i] > Str2[i])
-   	{
-   		printf("\n str2 is Less than str1");
-	}
-	else
-   	{
-   		printf("\n str1 is Equal to str2");
-	}
-  	
-  	return 0;
-}
+#include <stdio.h>  
+int compare(char[],char[]);  
+int main()  
+{  
+   char str1[20]; // declaration of char array  
+   char str2[20]; // declaration of char array  
+   printf("Enter the first string : ");  
+   scanf("%s",str1);  
+   printf("Enter the second string : ");  
+   scanf("%s",str2);  
+   int c= compare(str1,str2); // calling compare() function  
+   if(c==0)  
+   printf("strings are same");  
+   else  
+   printf("strings are not same");  
+  
+    return 0;  
+}  
+    
+int compare(char a[],char b[])  
+{  
+    int flag=0,i=0;  // integer variables declaration  
+    while(a[i]!='\0' &&b[i]!='\0')  // while loop  
+    {  
+       if(a[i]!=b[i])  
+       {  
+           flag=1;  
+           break;  
+       }  
+       i++;  
+    }  
+    if(flag==0)  
+    return 0;  
+    else  
+    return 1;  
+} 
