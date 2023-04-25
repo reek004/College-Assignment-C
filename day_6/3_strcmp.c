87aad499370e707afd5c6880b375b8c83456c16a
reek004 <chatterjeeswarnabha@gmail.com>
2023-04-25 10:10:16 +0530

    lgtm

--- day_6/3_strcmp.c
+++ day_6/3_strcmp.c
@@ -2,13 +2,13 @@
 int compare(char[],char[]);  
 int main()  
 {  
-   char str1[20]; // declaration of char array  
-   char str2[20]; // declaration of char array  
+   char str1[20];   
+   char str2[20];   
    printf("Enter the first string : ");  
    scanf("%s",str1);  
    printf("Enter the second string : ");  
    scanf("%s",str2);  
-   int c= compare(str1,str2); // calling compare() function  
+   int c= compare(str1,str2);  
    if(c==0)  
    printf("strings are same");  
    else  
